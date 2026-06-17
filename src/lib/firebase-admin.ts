d0e06f5950a3df98b6578c25ddedb3c4972cc237
seeken-dai <nick_lim@foxmail.com>
2026-06-17 18:53:34 +0800

    Set Firebase Admin project id

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -2,7 +2,7 @@ import { initializeApp, getApps, getApp } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getAuth } from 'firebase-admin/auth';
 
-const app = !getApps().length ? initializeApp() : getApp();
+const app = !getApps().length ? initializeApp({ projectId: 'countbottle-web' }) : getApp();
 
 export const adminDb = getFirestore(app);
 export const adminAuth = getAuth(app);
